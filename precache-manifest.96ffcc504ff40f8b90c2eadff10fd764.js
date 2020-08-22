ea83c816426c2ad7cc34a7b3578fd779fcb838dc
Jakhongir <jakhongirrasulov001@gmail.com>
2020-08-22 23:40:20 +0500

    Updates

---
+++ precache-manifest.96ffcc504ff40f8b90c2eadff10fd764.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4721604d7332400c888737a516e98a1a",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "ec544636cef692445001",
+    "url": "/covid-19/static/css/main.08f19c9b.chunk.css"
+  },
+  {
+    "revision": "8bb48af4ada9d03c060b",
+    "url": "/covid-19/static/js/2.a8d8bac3.chunk.js"
+  },
+  {
+    "revision": "ed24500623474e7d749065f68e230f46",
+    "url": "/covid-19/static/js/2.a8d8bac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec544636cef692445001",
+    "url": "/covid-19/static/js/main.de644fee.chunk.js"
+  },
+  {
+    "revision": "ca6f7991abbe432f93f7",
+    "url": "/covid-19/static/js/runtime-main.8e18646f.js"
+  }
+]);
